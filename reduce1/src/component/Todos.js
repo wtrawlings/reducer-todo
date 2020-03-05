349901ff164fc3f370249f2b07cdc7500432c14d
wtrawlings <rawlings.warren@gmail.com>
2020-03-04 19:17:22 -0500

    hit completion

--- reduce1/src/component/Todos.js
+++ reduce1/src/component/Todos.js
@@ -1,20 +1,41 @@
-import React, { useReducer }from "react";
+import React, { useReducer, useState }from "react";
 import { initialState, titleReducer } from "../reducers/reducer";
-
+import Task from "./Task"
 const Todos = () => {
     const [ state, dispatch ] = useReducer(titleReducer, initialState)
+    //is this deconstructing the state and dispatch?
+    const [todo, setTodo ]= useState("")
     console.log(state)
+
+    const changeHandler = (e) => {
+        setTodo(e.target.value)
+        console.log(todo)
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch({type: "ADD_TODO", payload:todo})
+        //dispatch goes back to reducer.js
+        //we use this to choose ACTIONs (Type and Payload)
+    }
+
     return (
         <div>
             <p>Trying to make a todo list with useReducer</p>
             <form>
                 <input type="text" name="taskText" 
-                //value={this.state.taskText} 
-                //onChange={this.changeHandler} 
+                value={todo} 
+                onChange={changeHandler} 
                 //had to turn these off to let form show in app
                 />
-                <button type="submit">Add</button>
+                <button type="submit" onClick={submitHandler} >Add</button>
             </form>
+            {state.map(task => {
+                //console.log(task);  
+                return <Task task={task.item} key={task.id} 
+                //toggleTask={propstoggleTask}
+                 />
+               
+            })}
         </div>
     );
 };
--- reduce1/src/reducers/reducer.js
+++ reduce1/src/reducers/reducer.js
@@ -17,12 +17,18 @@ export const initialState = [
 ];
   //I'm not sure what titleReducer is
   //I'm not sure what I'm missing here
+  //staring at this is making me worse
   export const titleReducer = (state, action) => {
     switch (action.type) {
       case "CHANGE_TITLE":
         return { ...state, title: action.payload, editing: false };
       case "CHANGE_EDIT":
         return { ...state, editing: true };
+        case "ADD_TODO":
+          console.log()
+          return [...state, {completed: false, id:Date.now, item: action.payload}];
+          //this return is our add to the text
+          //this was selected by the action type
       default:
         return state;
     }
